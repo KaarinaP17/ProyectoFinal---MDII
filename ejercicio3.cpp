fc41614c7a101d17ddce0befa8b707413d38ce0d
KaarinaP17 <00125123@uca.edu.sv>
2023-11-28 21:08:19 -0600

    :memo: Codigos de Proyecto Final

---
+++ ejercicio3.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <clocale> //Libreria que contiene la funcion de setLocale
+
+using namespace std;
+
+float probabilidadGeneral();
+float probabilidadT2();
+float probabilidadT3();
+void menu();
+
+int main()
+{
+
+    setlocale(LC_ALL, "spanish"); // componente para compilar las tildes en la terminal.
+    menu();
+    return 0;
+}
+
+void menu()
+{
+    int opc;
+
+    cout << "\n----- Bienvenido al programa que te brindara las probabilidades del siguiente ejercicio -----\n"
+         << endl;
+    cout << "Consideremos que el 40% de una fabrica proviene del T1, el 35% del turno T2 y el 25% del T3; y que los porcentajes de los articulos defectuosos son del 1%, 2% y 3%. Respectivamente, en cada turno, al seleccionar un articulo al azar de la produccion total: " << endl;
+    cout << "a. ¿Cual es la probabilidad de que resulte defectuoso?" << endl;
+    cout << "b. Si el articulo resulta defectuoso, ¿Cual es la probabilidad de que haya sido producido en el turno T2?" << endl;
+    cout << "c. ¿Cual es la probabilidad de que haya sido producido en el turno T3\n"
+         << endl;
+    do
+    {
+        cout << "1- Mostrar las probabilidades. " << endl
+             << "2- Salir del programa. \n"
+             << endl;
+        cout << "Introduce el numero de la opcion de la cual deseas conocer" << endl;
+        cout << "Opcion: ";
+        cin >> opc;
+
+        switch (opc)
+        {
+        case 1:
+            cout << "\n\nUsuario, te presentamos las posibles probabilidades: \n"
+                 << endl;
+            cout << "-------------------------------- PROBABILIDADES ------------------------------" << endl;
+            cout << "   |" << endl;
+            cout << "   |--- La probabilidad General es de: " << probabilidadGeneral() << endl;
+            cout << "   |   |" << endl;
+            cout << "   |   |--- La probabilidad que sea defectuoso especificamente de T2 " << endl;
+            cout << "   |       |" << endl;
+            cout << "   |       |    |--- Su probabilidad es de: " << probabilidadT2() << endl;
+            cout << "   |       |" << endl;
+            cout << "   |       |    |   |--- La probabilidad que sea defectuoso especificamente de T3 " << endl;
+            cout << "   |       |     " << endl;
+            cout << "   |       |    |   |--- Su probabilidad es de: " << probabilidadT3() << endl;
+            cout << "   |\n\n\n" << endl;
+            break;
+
+        case 2:
+            cout << "Usuario, ha salido del programa. Gracias por utilizarlo" << endl;
+            break;
+        default:
+            break;
+        }
+
+    } while (opc != 2);
+}
+
+float probabilidadGeneral()
+{
+    // calculamos la probabilidad total de que un articulo seleccionado al azar sea defectuoso:
+    float P_T = 0.40;                // probabilidad de la produccion proveniente de T
+    float P_T2 = 0.35;               // probabilidad de la produccion proveniente de 1T2
+    float P_T3 = 0.25;               // probabilidad de la produccion proveniente de T2
+    float P_Defectuoso_en_T = 0.01;  // probabilidad de defectuoso en T
+    float P_Defectuoso_en_T2 = 0.02; // probabilidad de defectuoso en 1T2
+    float P_Defectuoso_en_T3 = 0.03; // probabilidad de defectuoso en T2
+    float P_DefectuosoGeneral;
+    // P_Defectuoso = (P_T * P_Defectuoso_en_T + P_T2 * P_Defectuoso_en_T2 + P_T3 * P_Defectuoso_en_T3);
+    P_DefectuosoGeneral = ((P_T * P_Defectuoso_en_T) + (P_T2 * P_Defectuoso_en_T2) + (P_T3 * P_Defectuoso_en_T3));
+    return P_DefectuosoGeneral;
+}
+
+float probabilidadT2()
+{
+    float P_T2 = 0.35;
+    float P_Defectuoso_en_T2 = 0.02; // probabilidad de defectuoso en 1T2
+    float P_DefectuosoGeneral;
+    P_DefectuosoGeneral = ((0.40 * 0.01) + (0.35 * 0.02) + (0.25 * 0.03));
+    float defectuosoT2;
+    defectuosoT2 = ((P_T2 * P_Defectuoso_en_T2) / P_DefectuosoGeneral);
+    return defectuosoT2;
+}
+
+float probabilidadT3()
+{
+    float P_T3 = 0.25;
+    float P_Defectuoso_en_T3 = 0.03; // probabilidad de defectuoso en T2
+    float P_DefectuosoGeneral;
+    P_DefectuosoGeneral = ((0.40 * 0.01) + (0.35 * 0.02) + (0.25 * 0.03));
+    float defectuosoT3;
+    defectuosoT3 = ((P_T3 * P_Defectuoso_en_T3) / P_DefectuosoGeneral);
+    return defectuosoT3;
+}
---
+++ monedas.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <locale.h> //Libreria que contiene la funcion setlocale.
+#include <string>   //Libreria que contiene la función para manipular cadenas.
+
+using namespace std;
+
+float MonedaAmaniada(); // Probabilidad total de que la moneda sea amañada
+float Moneda1();        // Probabilidad de que a seleccionar la primera moneda, esta salga amañanada
+float Moneda2();        // Probabilidad de que a seleccionar la segunda moneda, esta salga amañanada
+float Moneda3();        // Probabilidad de que a seleccionar la tercera moneda, esta salga amañanada
+void menu();            // Menu de opciones para el programa
+
+int main()
+{
+    setlocale(LC_ALL, "spanish"); // componente para compilar las tildes en la terminal.
+
+    menu();
+
+    return 0;
+}
+
+void menu()
+{
+
+    int respuesta;
+    cout << "Una caja contiene tres monedas, dos de ellas normales (HT) y una amañada (HH). Una moneda se selecciona al azar y se lanza dos veces.Si H aparece en ambas ocasiones, ¿Cuál es la probabilidad de que la moneda seleccionada sea la amaniada?.";
+    cout << "\n----- Bienvenido al programa que detectara la probabilidad de que una moneda sea amaniada -----\n" << endl;
+
+    do
+    {
+        cout << "Introduce el número de la opción de la cuál deseas conocer" << endl;
+        cout << "1. Mostrar las probabilidades. " << endl;
+        cout << "2. Salir del programa" << endl;
+        cout << "Opción: ";
+        cin >> respuesta;
+
+        switch (respuesta)
+        {
+
+        case 1:
+            cout << "\n----------------------------------- PROBABILIDADES --------------------------------------- \n"
+                 << endl;
+            cout << "----- Aqui se calculara la probabilidad total de que la moneda sea amaniada" << endl;
+            cout << "   |" << endl;
+            cout << "   |-- Su probabilidad es de: " << MonedaAmaniada() << endl;
+            cout << "   |   |" << endl;
+            cout << "   |   |-- Aqui se calculara la probabilidad de que la primera moneda sea amaniada " << endl;
+            cout << "   |       |" << endl;
+            cout << "   |       |-- Su probabilidad es de: " << Moneda1() << endl;
+            cout << "   |       |" << endl;
+            cout << "   |       |   |-- Aqui se calculara la probabilidad de que la segunda moneda sea amaniada " << endl;
+            cout << "   |            " << endl;
+            cout << "   |       |   |-- Su probabilidad es de: " << Moneda2() << endl;
+            cout << "   |       |" << endl;
+            cout << "   |       |   |   |-- Aqui se calculara la probabilidad de que la tercer moneda sea amaniada " << endl;
+            cout << "   |            " << endl;
+            cout << "   |       |   |   |-- Su probabilidad es de: " << Moneda3() << endl;
+            cout << "   |       |" << endl;
+            cout << "   |" << endl;
+            cout << "\n\n";
+            break;
+
+        case 2:
+            cout << "Usuario, ha salido del programa. Gracias por utilizarlo!";
+            break;
+
+        default:
+            break;
+        }
+    } while (respuesta != 2);
+}
+
+float MonedaAmaniada()
+{
+    float probMoneda1 = Moneda1();
+    float probMoneda2 = Moneda2();
+    float probMoneda3 = Moneda3();
+
+    float ProbabilidadTotal = (0.3333) / (probMoneda1 + probMoneda2 + probMoneda3);
+    return ProbabilidadTotal;
+}
+
+float Moneda1()
+{
+    float ProbabilidadPrimeraSeleccion = 0.3333;
+    float ProbabilidadSegundaSeleccion = 0.5;
+    float ProbabilidadTerceraSeleccion = 0.5;
+    float ProbabilidadAmanio1;
+
+    ProbabilidadAmanio1 = ProbabilidadPrimeraSeleccion * ProbabilidadSegundaSeleccion * ProbabilidadTerceraSeleccion;
+    return ProbabilidadAmanio1;
+}
+
+float Moneda2()
+{
+
+    float ProbabilidadPrimeraSeleccion = 0.3333; // Probabilidad de seleccionar 1 moneda entre 3 opciones
+    float ProbabilidadSegundaSeleccion = 0.5;    // Una vez seleccionada la primera moneda, solo hay 2 posibilidades para seleccionar la siguiente
+    float ProbabilidadTerceraSeleccion = 0.5;    // Una vez seleccionada la primera moneda, solo hay 2 posibilidades para seleccionar la siguiente, al igual que en la segunda seleccion
+    float ProbabilidadAmanio2;                   // Resultado de la probabilidad de que la segunda moneda seleccionada sea amañanada
+
+    ProbabilidadAmanio2 = ((ProbabilidadPrimeraSeleccion) * (ProbabilidadSegundaSeleccion) * (ProbabilidadTerceraSeleccion));
+    return ProbabilidadAmanio2;
+}
+
+float Moneda3()
+{
+
+    float ProbabilidadPrimeraSeleccion = 0.3333; // Solo hay una opcion en este caso para seleccionar la moneda
+    float ProbabilidadAmanio3;                   // Resultado de la probabilidad de que la tercera moneda seleccionada sea amañanada
+
+    ProbabilidadAmanio3 = ProbabilidadPrimeraSeleccion;
+    return ProbabilidadAmanio3;
+}
